89fe352524e13157a5ec74087acd1e0aa4db1455
Aman Anand <109837285+amananand19@users.noreply.github.com>
2023-10-01 15:58:43 +0000

    variable

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 45896
+let accountEmail = "amananandyaduwanshi@google.com"
+var accountPassword = "12345"
+accountCity = "Madhubani"
+let accountState;
+
+// accountId =2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "569425"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
