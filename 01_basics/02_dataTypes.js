aa85006cbd8df614d1353b4145ce04397437b565
Aman Anand <109837285+amananand19@users.noreply.github.com>
2023-10-02 02:45:42 +0530

    Create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Aman")
+
+let name = "Aman"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object 
+
+console.log(typeof "Aman")
+console.log(typeof age)
+console.log(typeof symbol)
+console.table([typeof "Aman", typeof age, typeof isLoggedIn])
